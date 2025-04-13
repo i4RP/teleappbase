9ac162b76319edf5e2fe02fe552a4ea836712e4c
dar <shoei0205@gmail.com>
2025-04-14 00:41:39 +0900

    fix3: Compatibility flags

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <html>
       <body>
-        <h1>Page Not Found (404)</h1>
+        <h1>Page Not Found (404) </h1>
       </body>
     </html>
   );
