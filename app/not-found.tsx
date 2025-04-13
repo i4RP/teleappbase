c75122fe1caf1ddc46bd2f9bbbb4ef4a8ccf3b53
dar <shoei0205@gmail.com>
2025-04-13 22:58:22 +0900

    fix: add edge runtime to not-found

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+export const runtime = 'edge';
+
+export default function NotFound() {
+  return (
+    <html>
+      <body>
+        <h1>Page Not Found (404)</h1>
+      </body>
+    </html>
+  );
+}
