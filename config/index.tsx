d6b6ec1883197007831d137ce8ca5568de807c16
dar <shoei0205@gmail.com>
2025-04-30 08:54:38 +0900

    r5

--- config/index.tsx
+++ config/index.tsx
@@ -1,6 +1,10 @@
-import { cookieStorage, createStorage, http } from '@wagmi/core'
+import { webStorage, createStorage, http } from '@wagmi/core'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { mainnet, arbitrum, scroll, morph, berachainTestnetbArtio, mantle, soneium, zircuit, rootstock, abstract, viction, monadTestnet, celo, apeChain} from '@reown/appkit/networks'
+import {
+  mainnet, arbitrum, scroll, morph, berachainTestnetbArtio,
+  mantle, soneium, zircuit, rootstock, abstract, viction,
+  monadTestnet, celo, apeChain
+} from '@reown/appkit/networks'
 
 // Get projectId from https://cloud.reown.com
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
@@ -9,14 +13,18 @@ if (!projectId) {
   throw new Error('Project ID is not defined')
 }
 
-export const networks = [mainnet, arbitrum, scroll, morph, berachainTestnetbArtio, mantle, soneium, zircuit, rootstock, abstract, viction, monadTestnet, celo, apeChain]
+export const networks = [
+  mainnet, arbitrum, scroll, morph, berachainTestnetbArtio,
+  mantle, soneium, zircuit, rootstock, abstract, viction,
+  monadTestnet, celo, apeChain
+]
 
-//Set up the Wagmi Adapter (Config)
+// Set up the Wagmi Adapter using webStorage (localStorage internally)
 export const wagmiAdapter = new WagmiAdapter({
   storage: createStorage({
-    storage: localStorage
+    storage: webStorage
   }),
-  ssr: true,
+  ssr: false, // もしエラーが出るなら false にしてみてください
   networks,
   projectId
 })
